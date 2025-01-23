8120d47ccadbea35a34f1ea0092b1d1e596afb86
DenisAnufriev <wenomavel89@gmail.com>
2025-01-23 14:00:50 +0300

    add docker update README.md update settings

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-@500uc*9wy1#*p0a3m4%gl#$j=g1e9tqx1tq4=2m$=0qxa3wod'
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
